193eec37893e2d39949bc541da21a68b85e53be2
Sanja Mulla <sanjamulla03@gmail.com>
2022-05-22 13:12:04 +0200

    feature create component trips /  bootstrap INSTALLED
    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExampleComponent } from './example/example.component';
+import { TripsComponent } from './trips/trips.component';
 
 const routes: Routes = [
   { path: 'example', component: ExampleComponent },
+  { path: 'trips', component: TripsComponent }
 ];
 
 @NgModule({
