098ec64c7c6a88651a448357e7df983a1c356941
Thoma <hmimani@outlook.com>
2022-06-01 02:12:06 +0200

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ExampleComponent } from './example/example.component';
 import { TripsComponent } from './trips/trips.component';
 import { HomeContentComponent } from './home-content/home-content.component';
+import { JoinOurTeamComponent } from './join-our-team/join-our-team.component';
 
 const routes: Routes = [
   { path: '', component: HomeContentComponent },
   { path: 'example', component: ExampleComponent },
-  { path: 'trips', component: TripsComponent }
+  { path: 'trips', component: TripsComponent },
+  { path: 'join-our-team', component: JoinOurTeamComponent }
 
   // { path: 'trips/:id', component: SingleTripComponent },
 ];
