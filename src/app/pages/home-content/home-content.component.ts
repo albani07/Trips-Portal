a95581b9131d61bee80f3c659541359338ee1a55
alban <alban07halaj@gmail.com>
2022-06-05 13:15:03 +0200

    feat (done: method for popular trips and upcoming trips) wip

--- src/app/pages/home-content/home-content.component.ts
+++ src/app/pages/home-content/home-content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Trip } from 'src/app/interfaces/trip';
+import { TripService } from 'src/app/services/trip.service';
 
 @Component({
   selector: 'app-home-content',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-content.component.scss']
 })
 export class HomeContentComponent implements OnInit {
+  mostPopular: Trip[];
+  upComing: Trip[];
+  tripService: TripService;
 
-  constructor() { }
+  constructor(tripService: TripService) {
+    this.tripService = tripService;
+    this.mostPopular = tripService.popular();
+    this.upComing = tripService.upcoming();
+  }
 
   ngOnInit(): void {
   }
