c68d0fc51bf3ffa92e157b121b06bdf028ab5575
dorisrapi <dorisrapi19@gmail.com>
2022-06-05 15:07:11 +0200

    feat(1. Added a GoogleFont in index.html, 2. Updated the whole singleTrip page)

--- src/app/trips/single-trip/single-trip.component.ts
+++ src/app/trips/single-trip/single-trip.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Trip } from 'src/app/interfaces/trip';
 
 @Component({
   selector: 'app-single-trip',
@@ -6,24 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-trip.component.scss'],
 })
 export class SingleTripComponent implements OnInit {
-  imagePath1: string;
-  imagePath2: string;
-  imagePath3: string;
-  imagePath4: string;
-  locationIcon: string;
-  // oiaImg: string;
-  // quadImg: string;
-  currentRate = 9.6;
-  public isCollapsed = true;
-  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`); //in cases of having a Database where all the images are stored
+  @Input() trip!: Trip;
+
   constructor() {
-    this.imagePath1 = '../../assets/images/single-trip-pictures/theth1.jpg';
-    this.imagePath2 = '../../assets/images/single-trip-pictures/theth2.jpg';
-    this.imagePath3 = '../../assets/images/single-trip-pictures/theth3.jpg';
-    this.imagePath4 = '../../assets/images/single-trip-pictures/theth4.jpg';
-    this.locationIcon = '../../assets/images/single-trip-pictures/location.png';
-    // this.oiaImg = '../../assets/images/oia.jpg';
-    // this.quadImg = '../../assets/images/fira.jpg';
+    // this.imagePath1 = '../../assets/images/single-trip-pictures/theth1.jpg';
+    // this.imagePath2 = '../../assets/images/single-trip-pictures/theth2.jpg';
+    // this.imagePath3 = '../../assets/images/single-trip-pictures/theth3.jpg';
+    // this.imagePath4 = '../../assets/images/single-trip-pictures/theth4.jpg';
+    // this.locationIcon = '../../assets/images/single-trip-pictures/location.png';
+    // // this.oiaImg = '../../assets/images/oia.jpg';
+    // // this.quadImg = '../../assets/images/fira.jpg';
   }
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 }
